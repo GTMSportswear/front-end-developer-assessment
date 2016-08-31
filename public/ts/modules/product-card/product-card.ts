4ce8077e91b6cab7461f7aaa59b1d6a294b803c3
Nick George <me@izzmo.com>
2016-08-31 12:11:02 -0500

    Quick final update.

--- public/ts/modules/product-card/product-card.ts
+++ public/ts/modules/product-card/product-card.ts
@@ -4,15 +4,4 @@ export class ProductCard {
   constructor(private viewEngine: IViewEngine) {
     // using dependency injection for the view engine
   }
-  
-  public output(): Promise<Element> {
-    return new Promise<Element>((success, error) => {
-      this.viewEngine.get('/js/modules/product-card/view.html', { name: 'Test' })
-        .then((view: View) => {
-          // View is just an abstraction of a Node Element.
-          success(view);
-        })
-        .catch(error);
-    });
-  }
 }
